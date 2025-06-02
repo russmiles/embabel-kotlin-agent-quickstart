2e1e9d1c9c1b8909dbedc0a9c63bda5522f62ea9
Russ Miles <russ@russmiles.com>
2025-06-02 15:33:47 +0100

    First version of Embabel Java agent GH template

---
+++ src/main/kotlin/com/russmiles/EmbabelAgentApplication.kt
@@ -0,0 +1,11 @@
+package com.russmiles
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmbabelAgentApplication
+
+fun main(args: Array<String>) {
+	runApplication<EmbabelAgentApplication>(*args)
+}
